8dbaa8848a3bdbf5f794d64ff3dcc03c4c7e7ed4
cristiansennait <cristiansennait@gmail.com>
2020-02-10 12:07:10 -0300

    cambiando color de h1

--- practica.js
+++ practica.js
@@ -1,10 +1,1 @@
-const numero = 0;
-function contarHastaDiez() {
-    var text = "",
-  var i = 0;
-  while (i < 10) {
-    text =
-    i++;
-  }
-}
-document.write(contarHastaDiez);
+console.log("Funcionando perfectamente");
